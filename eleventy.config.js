aecfdc286299850455321e433fa22ca4df3c8346
chalatz <chralatz@gmail.com>
2025-05-22 14:22:19 +0300

    Added favicon

--- eleventy.config.js
+++ eleventy.config.js
@@ -51,6 +51,14 @@ export default function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy('./images/**/*');
 
+  eleventyConfig.addPassthroughCopy('./android-chrome-192x192.png');
+  eleventyConfig.addPassthroughCopy('./android-chrome-512x512.png');
+  eleventyConfig.addPassthroughCopy('./apple-touch-icon.png');
+  eleventyConfig.addPassthroughCopy('./favicon-16x16.png');
+  eleventyConfig.addPassthroughCopy('./favicon-32x32.png');
+  eleventyConfig.addPassthroughCopy('./favicon.ico');
+  eleventyConfig.addPassthroughCopy('./site.webmanifest');
+
   eleventyConfig.addPassthroughCopy("CNAME");
 
   eleventyConfig.addDataExtension("yaml", (contents) => yaml.load(contents));
